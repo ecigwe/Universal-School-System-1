9e0612bcf1229a6a3690d0a54ff74304b2a36eaa
IMMANUEL5015 <immanueldiai@gmail.com>
2020-05-22 19:42:06 +0100

    Handling CastError In A Production Environment

--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -1,6 +1,13 @@
 const handleDuplicateFieldsError = (err) => {
     const value = err.errmsg.match(/(["'])(?:\\.|[^\\])*?\1/)[0];
     err.message = `${value} already in use. Please use something else.`;
+    err.isOperational = true;
+    return err;
+}
+
+const handleCastError = (err) => {
+    err.message = `We are unable to find what you are looking for!`;
+    err.isOperational = true;
     return err;
 }
 
@@ -13,9 +20,15 @@ const sendErrorInDevMode = (error, request, response) => {
 }
 
 const sendErrorInProdMode = (err, request, response) => {
-    return response.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
+    if (err.isOperational) {
+        return response.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    }
+    return response.status(500).json({ //Programming errors
+        status: 'error',
+        message: 'Something went very wrong.'
     });
 }
 
@@ -30,6 +43,8 @@ const globalErrorHandler = (error, request, response, next) => {
         err.message = error.message;
 
         if (err.code === 11000) err = handleDuplicateFieldsError(err);
+        if (err.name === 'CastError') err = handleCastError(err);
+
         return sendErrorInProdMode(err, request, response); //Send Well Crafted Error To Our Clients
     }
 }
