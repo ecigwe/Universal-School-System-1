347f645937986381188f063b976212d032d8ce52
IMMANUEL5015 <immanueldiai@gmail.com>
2020-05-22 10:02:00 +0100

    Fix Bug With Admin Signup

--- controllers/authentication/admin.js
+++ controllers/authentication/admin.js
@@ -3,19 +3,23 @@ const errorHandler = require('../../utils/errorHandler');
 
 exports.register = async (request, response, next) => {
     try {
-        const newAdmin = await Admin.create({
-            fullname: request.body.fullname,
-            email: request.body.email,
-            username: request.body.username,
-            role: request.body.role,
-            phoneNumber: request.body.phoneNumber,
-            password: request.body.password,
-            confirmPassword: request.body.confirmPassword,
-            isAnAdmin: request.body.adminCode === process.env.ADMIN_CODE
-        });
-        request.user = newAdmin;
-        response.statusCode = 201;
-        next();
+        if (request.body.adminCode === process.env.ADMIN_CODE) {
+            const newAdmin = await Admin.create({
+                fullname: request.body.fullname,
+                email: request.body.email,
+                username: request.body.username,
+                role: request.body.role,
+                phoneNumber: request.body.phoneNumber,
+                password: request.body.password,
+                confirmPassword: request.body.confirmPassword,
+                isAnAdmin: true
+            });
+            request.user = newAdmin;
+            response.statusCode = 201;
+            next();
+        } else {
+            return errorHandler(400, 'Please enter the correct admin code');
+        }
     } catch (error) {
         console.log(error);
         next(error);
