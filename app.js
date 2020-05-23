650c619dcde189da25f0f7f19f86cc64f00c8d8d
IMMANUEL5015 <immanueldiai@gmail.com>
2020-05-23 03:00:21 +0100

    Implement Cross Origin Resource Sharing

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const sanitizeNosqlQuery = require('express-mongo-sanitize');
 const preventCrossSiteScripting = require('xss-clean');
 const preventParameterPollution = require('hpp');
 const compression = require('compression');
+const cors = require('cors');
 const router = require('./routes/router');
 
 const app = express();
@@ -18,6 +19,9 @@ mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true
 }).then(con => console.log(`Connected to ${con.connections[0].name} Database successfully`))
     .catch(error => { return console.log(error); });
+
+app.use(cors());
+app.options('*', cors());
 
 app.use(helmet());
 app.use('/api', rateLimiter({
