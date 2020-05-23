887d46f3e00bb06c00f3d61652504fc43bec34a3
Abuchi Kingsley <abuchikingsley76@gmail.com>
2020-05-23 13:50:30 +0100

    Added admins to schools

---
+++ dev-data/admins.js
@@ -0,0 +1,13 @@
+[
+    {
+        "fullname": "Abuchi Kingsley Ndinigwe",
+        "email": "abuchikings@hotmail.com",
+        "username": "abuchikingsley76",
+        "phoneNumber": "08062158380",
+        "role": "backend-developer",
+        "category": "admin",
+        "isAnAdmin": true,
+        "password": "qwerty12345",
+        "confirmPassword": "qwerty12345"
+    }
+]
