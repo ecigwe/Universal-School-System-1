fc205c0f8bf02b72c39c168615e8c58dd3e1333f
IMMANUEL5015 <immanueldiai@gmail.com>
2020-05-23 01:48:35 +0100

    Prevent DDOS Attack On Our API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const rateLimiter = require('express-rate-limit');
 const router = require('./routes/router');
 
 const app = express();
@@ -11,7 +12,13 @@ mongoose.connect(process.env.MONGODB_URI, {
     useFindAndModify: false,
     useUnifiedTopology: true
 }).then(con => console.log(`Connected to ${con.connections[0].name} Database successfully`))
-    .catch(error => {return console.log(error); });
+    .catch(error => { return console.log(error); });
+
+app.use('/api', rateLimiter({
+    max: 1000,
+    windowMs: 1000 * 60 * 60,
+    message: 'Too many requests from this IP. Try again in an hour.'
+}));
 
 app.use(express.json({ limit: '20kb' }));
 app.use(cookieParser());
